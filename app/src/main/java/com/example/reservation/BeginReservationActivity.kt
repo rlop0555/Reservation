506a2223515f0332113f5ac9d3bd29cb42e9b6bd
Christian Montemayor <christianamontemayor@gmail.com>
2022-11-20 16:38:34 -0600

    home page commit

---
+++ app/src/main/java/com/example/reservation/BeginReservationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.reservation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BeginReservationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_begin_reservation)
+    }
+}
--- app/src/main/java/com/example/reservation/MainActivity.kt
+++ app/src/main/java/com/example/reservation/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.reservation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnLogin: Button = findViewById(R.id.btnLogin)
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, LogInActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnGuest: Button = findViewById(R.id.btnGuest)
+        btnGuest.setOnClickListener {
+            val intent = Intent(this, BeginReservationActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
